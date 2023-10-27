600dfc4af85bc6d698ad0d365af683f462cbb51c
Shahmir-Muhammad <147588981+Shahmir-Muhammad@users.noreply.github.com>
2023-10-27 02:48:44 -0700

    Add files via upload

---
+++ lab task 4 by shahmir.cpp
@@ -0,0 +1,75 @@
+//TASK 1
+/*#include<iostream>
+using namespace std;
+int main (){
+	int i=1,sum = 0;
+	while(i<=10){
+		sum = sum + i;
+		i++;
+		}
+cout << "The sum of first 10 natural numbers is : "<< sum;
+}*/
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+
+
+
+//TASK 2
+
+
+
+/*
+	int x=0, y=0, z=0;
+	cout<<"enter number"<<endl;
+	cin>>y;
+
+	for(x=1; x<10;x++){
+	
+	z=y*x;
+	cout<<y<<"*"<<x<<"="<<z;
+}
+	return 0;
+}*/
+
+
+
+
+//TASK 3
+/*   
+{
+	int a;
+	cout <<"Write the number to get factorial :"<<endl;
+	cin >>a;
+	int b=1;
+	for(a;a>0;a--){
+	b=b*a;cout<<b<<endl;
+	}cout<<b<<endl;
+	return 0;
+	
+	}
+	{*/
+	
+	
+	//TASK 4
+	
+/*	int a=0, b=0, c=0, d=1, add=0;
+	cout<<"entering numbers to generate fibonacci sequence"<<b<<endl;
+	cin>>b;
+	cout<<"0, 1, ";
+	for(a=0;a<(b-2);a++){
+		
+		add=c+d;
+
+	cout<<add<<", ";
+	c=d;
+	d=add;
+	add=0;
+	}
+	return 0;
+	}*/
+	
+	
+	
